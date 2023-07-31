fbd5e0008271051c0b03ed9a252e53672e64ca13
rhoolan <rhoolan@gmail.com>
2023-07-31 14:41:22 +0900

    Submit practice 1

---
+++ Day_03_JS/practice1_isPrime/javascript.js
@@ -0,0 +1,34 @@
+function checkPrime() {
+    var value = getValue();
+    var result = isPrime(value);
+    displayAnswer(value, result);
+}
+
+function isPrime(number) {
+    if (number == 0 || number == 1) {
+        return false;
+    } else if (number == 2) {
+        return true;
+    } else {
+        for (let i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false
+            }
+        }
+        return true;
+    }
+}
+
+function getValue() {
+    var input = document.getElementById("value").value;
+    return input;
+}
+
+function displayAnswer(value, answer){
+    if (answer) {
+        document.getElementById("answer").innerHTML = value + " is prime";
+    } else {
+        document.getElementById("answer").innerHTML = value + " is not prime";
+    }
+
+}
